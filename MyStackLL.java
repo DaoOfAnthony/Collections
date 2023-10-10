81f8cd25d687d82984609f47e8dd7a8b9ad67a30
DaoOfAnthony <143449540+DaoOfAnthony@users.noreply.github.com>
2023-10-09 22:55:23 -0700

    Update MyStackLL.java

--- MyStackLL.java
+++ MyStackLL.java
@@ -12,20 +12,32 @@ public class MyStackLL<E> {
         stackData = new MyLinkedList<E>();
     }
     
+     /**
+     * Pushes element to stack
+     *
+     * @param element pushed 
+     */
     public void push(E element) {
         stackData.addHead(element);
-       
     }
     
+     /**
+     * checks if the stack is empty 
+     *
+     * @return if empty 
+     */
     public boolean isEmpty() {
-        if (stackData.head == null) {
-           return true; 
-        } else {
-            return false;
-        }
+        E temp = stackData.getHead();
         
+        stackData.removeHead();
+        return temp;
     }
     
+     /**
+     * Pop element from the stack
+     *
+     * @param element popped
+     */
     public int pop() {
         if (!isEmpty()) {
             return stackData.size();
@@ -34,28 +46,31 @@ public class MyStackLL<E> {
         }
     }
     
+    /**
+     * reads element at the top of the stack
+     *
+     * @return element stored at top
+     */
     public E top() {
         return stackData.head.getData();
     }
-    
+
+     /**
+     * returns the number of elements in the stack
+     *
+     * @return data in stack
+     */
     public int size() {
         return stackData.size();
     }
     
-    public boolean isFull() {
-        return false;
-    }
-    
+    /**
+     * returns element of the stack from top to bottom
+     *
+     * @return toString
+     */
     public String toString() {
-        Node curNode = stackData.head; 
-        String result = "";
-
-        for (int i = 0; i < stackData.size(); i++) {
-            result = result + curNode.getData() + " ";
-            curNode = curNode.getNext();
-        }
-    
-        return result;
+        return stackData.toString();
     }
 }
 
