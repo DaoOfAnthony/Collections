9b47ca4007275678e0eb730d6304c990053cbfc2
DaoOfAnthony <143449540+DaoOfAnthony@users.noreply.github.com>
2023-10-31 11:15:13 -0700

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,4 +1,3 @@
-
 /**
  * Write a description of class Node here.
  *
@@ -10,19 +9,26 @@ public class Node<E>
     E data;  
     Node<E> next;  
     
+    //constructor 
     public Node(E data) {  
         this.data = data;  
         this.next = null;  
     }  
     
+    //get next
     public Node<E> getNext()
     {
         return next;
     }
     
+    //get data
     public E getData()
     {
         return data;
     }
     
+    //sets next
+    public void setNext(Node<E> newNext) {
+        next = newNext;
+    }
 }
