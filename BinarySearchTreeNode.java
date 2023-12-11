00a4c269e858730bb6e64a9b794d53fc920b5f4e
DaoOfAnthony <143449540+DaoOfAnthony@users.noreply.github.com>
2023-12-10 20:38:28 -0800

    Rework V2

--- BinarySearchTreeNode.java
+++ BinarySearchTreeNode.java
@@ -1,72 +1,46 @@
-
 /**
- * Write a description of class BinarySearchTreeNode here.
+ * Implementation of BST Node
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Anthony
+ * @version V1
  */
-public class BinarySearchTreeNode<E extends Comparable <E>>
-{
-    // instance variables
+public class BinarySearchTreeNode<E extends Comparable<E>> {
     private BinarySearchTreeNode<E> left;
-    private BinarySearchTreeNode<E> right; 
-    private E data; 
-    
-    public BinarySearchTreeNode(E data)
-    {
-        left = null; 
-        right = null;
+    private BinarySearchTreeNode<E> right;
+    private E data;
+
+    public BinarySearchTreeNode(E data) {
         this.data = data;
+        left = null;
+        right = null;
     }
     
-    public E getData()
-    {
+    public E getData() {
         return data;
     }
     
-    public void setData(E newData)
-    {
+    public void setData(E newData) {
         data = newData;
     }
     
-    public BinarySearchTreeNode<E> getRight()
-    {
-        return right;
-    }
-    
-    public  BinarySearchTreeNode<E> setRight(BinarySearchTreeNode<E> newRight)
-    {
-        return right = newRight;
-    }
-    
-     public  BinarySearchTreeNode<E> getLeft()
-    {
+    public BinarySearchTreeNode<E> getLeft() {
         return left;
     }
     
-    public BinarySearchTreeNode setLeft( BinarySearchTreeNode<E> newLeft)
-    {
-        return left = newLeft;
+    public void setLeft(BinarySearchTreeNode<E> newLeft) {
+        left = newLeft;
     }
     
-     public E getMin() {
-        if(left == null) {
-            return data;
-        } else {
-            return left.getMin();
-        }
+    public BinarySearchTreeNode<E> getRight() {
+        return right;
     }
     
-    public E getMax() {
-        if(right == null) {
-            return data;
-        } else {
-            return right.getMax();
-        }
+    public void setRight(BinarySearchTreeNode<E> newRight) {
+        left = newRight;
     }
     
-     public void insert(E element) {
-        if (data.compareTo(element ) > 0) { // left
+    public void insert(E element) {
+        if (element.compareTo(data) <= 0) { // left
             if (left == null) {
                 left = new BinarySearchTreeNode<E>(element);
             } else {
@@ -78,6 +52,50 @@ public class BinarySearchTreeNode<E extends Comparable <E>>
             } else {
                 right.insert(element);
             }
+        }
+    }
+    
+    public BinarySearchTreeNode remove(E element) { 
+        if (data == element) { 
+            if (left == null && right == null) {
+                return null;
+            } else if (left != null && right == null) {
+                return left;
+            } else if (left == null && right != null){
+                return right;
+            } else { 
+                data = left.getMax();
+                left = left.removeMax();
+            }
+        } else {
+            if (element.compareTo(data) < 0) {
+                if (left != null) {
+                    left = left.remove(element);
+                }
+            } else {
+                if (right != null) {
+                    right = right.remove(element); 
+                }
+            }
+        }
+        return this;
+    }
+    
+    public BinarySearchTreeNode<E> removeMin() {
+        if (left == null) {
+            return right;
+        } else {
+            left = left.removeMin();
+            return this;
+        }
+    }
+    
+    public BinarySearchTreeNode<E> removeMax() { 
+        if (right == null) {
+            return left;
+        } else {
+            right = right.removeMin();
+            return this;
         }
     }
     
@@ -99,8 +117,24 @@ public class BinarySearchTreeNode<E extends Comparable <E>>
         }
     }
     
-     public int getDepth() {
-        if(left  == null && right == null) {
+    public E getMin() {
+        if (left == null) {
+            return data;
+        } else {
+            return left.getMin();
+        }
+    }
+    
+    public E getMax() {
+        if (right == null) {
+            return data;
+        } else {
+            return right.getMax();
+        }
+    }
+    
+    public int getDepth() {
+        if (left  == null && right == null) {
             return 0;
         } else if (left == null) {
             return right.getDepth() + 1;
@@ -116,72 +150,106 @@ public class BinarySearchTreeNode<E extends Comparable <E>>
         }
     }
     
-    public BinarySearchTreeNode<E> removeMin() { 
-        if(left == null) {
-            data = null;
-            return null;
-        } else {
-            left = left.removeMin();
-            return this;
-        }
-    }
-    
-    public BinarySearchTreeNode<E> removeMax() { // assume tree has values
-        if(right == null) {
-            data = null;
-            return null;
-        } else {
-            right = right.removeMin();
-            return this;
-        }
-    }
-    
-     public BinarySearchTreeNode remove(E element) { // assume element is in array
-         
-        if (element.compareTo(data) < 0) { 
-            if (left == null) {
-                return this;
-            } else {
-                left = left.remove(element);
-                return this;
-            }
-        } else if (element.compareTo(data) > 0) { 
-            if (right == null) {
-                return this;
-            } else {
-                right = right.remove(element);
-                return this;
-            }
-        } else { // match
-            if (left == null && right == null) { 
-                data = null;
-                return null;
-            } else if (right == null) {
-                data = null;
-                return left;
-            } else if (left == null) {
-                data = null;
-                return right;
-            } else {
-                data = left.getMax();
-                left.removeMax();
-                return this;
-            }
-        }
-    }
-    
-     public String toString() {
+    public String toString() {
         String returned = "";
         
         if (left != null) {
             returned = left.toString() + ", ";
         } 
-        
         returned = returned + data.toString();
-        
         if (right != null) {
             returned = returned + ", " + right.toString();
         }
         return returned;
+    }
+    
+     /**     * Prints elements as stored in the tree
+     *
+     * @param maxElementWidth The maximum space allowed for the string form
+     *                        of the element.
+     */
+    public void printTree(int maxElementWidth) {
+        int maxSize = (int) Math.pow(2, (getDepth() + 1));
+        String[] elements = new String[maxSize];
+
+        // Build array of elements
+        printTree(elements, 0, maxElementWidth);
+
+        // Print element properly spaced
+        int fullWidth = (int) Math.pow(2, getDepth()) * (maxElementWidth + 1);
+        for (int i = 0; i < getDepth() + 1; i++) {
+            String connectionsLevel = "";
+            String elementsLevel = "";
+
+            for (int j = (int) Math.pow(2, i) - 1; j < (int) Math.pow(2, i + 1) - 1; j++) {
+
+                // Process arrows for this level
+                String arrow = "  ";
+                int elementLength = arrow.length();
+                if (elements[j] != null) {
+                    if (j % 2 == 1) { // Odd is left child
+                        arrow = " /";
+                    } else { // Even is right child
+                        arrow = "\\ ";
+                    }
+                }
+
+                // Center within maxElementWidth
+                String leftPadStr = ""; // Default
+                String rightPadStr = ""; // Default
+                int leftPadNum = (fullWidth / (int) Math.pow(2, i) - elementLength) / 2;
+                int rightPadNum = fullWidth / (int) Math.pow(2, i) - elementLength - leftPadNum;
+                if (leftPadNum > 0) {
+                    leftPadStr = String.format("%" + leftPadNum + "s", " ");
+                }
+                if (rightPadNum > 0) {
+                    rightPadStr = String.format("%" + rightPadNum + "s", " ");
+                }
+                connectionsLevel += leftPadStr + arrow + rightPadStr;
+
+                // Process elements for this level
+                elementLength = 0;
+                if (elements[j] != null) {
+                    elementLength = elements[j].toString().length();
+                }
+
+                // Center within maxElementWidth
+                leftPadStr = ""; // Default
+                rightPadStr = ""; // Default
+                leftPadNum = (fullWidth / (int) Math.pow(2, i) - elementLength) / 2;
+                rightPadNum = fullWidth / (int) Math.pow(2, i) - elementLength - leftPadNum;
+                if (leftPadNum > 0) {
+                    leftPadStr = String.format("%" + leftPadNum + "s", " ");
+                }
+                if (rightPadNum > 0) {
+                    rightPadStr = String.format("%" + rightPadNum + "s", " ");
+                }
+
+                if (elements[j] != null) {
+                    elementsLevel += leftPadStr + elements[j] + rightPadStr;
+                } else {
+                    elementsLevel += leftPadStr + rightPadStr;
+                }
+            }
+
+            if (i > 0) { // Do not print arrows for root
+                System.out.println(connectionsLevel);
+            }
+            System.out.println(elementsLevel);
+        }
+    }
+
+    // build array of element strings
+    private void printTree(String[] elements, int nodeNum, int maxElementWidth) {
+        elements[nodeNum] = data.toString();
+        if (elements[nodeNum].length() > maxElementWidth) {
+            elements[nodeNum] = elements[nodeNum].substring(0, maxElementWidth);
+        }
+        if (left != null) {
+            left.printTree(elements, (nodeNum + 1) * 2 - 1, maxElementWidth);
+        }
+        if (right != null) {
+            right.printTree(elements, (nodeNum + 1) * 2, maxElementWidth);
+        }
     }
 }
